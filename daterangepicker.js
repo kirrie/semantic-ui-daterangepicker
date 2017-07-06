3c4de0aaa7994146d1fb04ee4a57fb59ea2c5296
kirrie <kirrie@gmail.com>
2017-07-06 15:40:54 +0900

    - change moment package name

--- daterangepicker.js
+++ daterangepicker.js
@@ -9,7 +9,7 @@
 (function (root, factory) {
 
     if(typeof define === 'function' && define.amd) {
-        define(['momentWithLocales', 'exports'], function (momentjs, exports) {
+        define(['moment-with-locales', 'exports'], function (momentjs, exports) {
             root.daterangepicker = factory(root, exports, momentjs, $);
         });
 
